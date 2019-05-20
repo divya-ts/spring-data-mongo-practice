60c17dd094850312cb38c2dfcd93a12522c216a0
unknown <sneha.tulsyan1192@gmail.com>
2019-05-20 16:36:58 +0530

    new new

--- Employee/src/main/java/com/aggregate/Employee/controller/EmployeeController.java
+++ Employee/src/main/java/com/aggregate/Employee/controller/EmployeeController.java
@@ -3,21 +3,20 @@ package com.aggregate.Employee.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.aggregate.Employee.model.employeeDetails;
-import com.aggregate.Employee.repository.employeeRepo;
 import com.aggregate.Employee.service.EmployeeService;
 
-@Controller
+@RestController
 public class EmployeeController {
 
 	@Autowired
 	EmployeeService employeeService;
 	
 	@GetMapping("/employees")
-	public List<employeeDetails> getInfo(){
-		return employeeRepository.Employees();
+	public List<employeeDetails> Employees(){
+		return employeeService.getInfo();
 	}
 }
